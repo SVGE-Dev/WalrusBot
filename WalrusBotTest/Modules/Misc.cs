29f812b794d5cc9084e7632d5fc01cee4f0498a2
MerrickKing <merrickkainking@gmail.com>
2018-12-27 14:14:27 +0000

    Initial commit of current test bot
    Test bot with functional react-based role giving. Has not yet been stress-tested or has permissions double-checked. Requires a re-write for smoothness; will print the message without first checking that it is all legal command syntax etc.

---
+++ WalrusBotTest/Modules/Misc.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace WalrusBotTest.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public Task DefaultReactRoleAsync()
+            => ReplyAsync("https://youtu.be/X2jjf_XRpKc?t=7");
+
+        [Command("info")]
+        public Task Info()
+            => ReplyAsync($"Hello, I am a bot called {Context.Client.CurrentUser.Username} written in Discord.Net 1.0.2\n");
+    }
+}
